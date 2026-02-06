1e9ddfd1b98ea51d5a75f216b8f17bd32e0dba0a
Samir Chowdhury <samirchowdhury0@proton.me>
2026-02-06 13:34:19 +0000

    background detection
    finds outer contour of paper in image

--- computer-vision/main.py
+++ computer-vision/main.py
@@ -7,10 +7,33 @@ from matplotlib import pyplot as plt
 from process_image import *
 
 
+def corner_detect(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray,5,1,0.04)
+    #result is dilated for marking the corners, not important
+    dst = cv2.dilate(dst,None)
+
+    # Threshold for an optimal value, it may vary depending on the image.
+    img[dst>0.01*dst.max()]=[0,0,255]
+
+    return img
+
+def mark_contours(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    blurred = cv2.GaussianBlur(gray, (5,5), 5)
+
+    edges = cv2.Canny(gray, 20, 20)
+
+    contours, image = cv2.findContours(edges, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    img = cv2.drawContours(img, contours, -1, (0,255,0), 3)
+    return img
+
 def main():
     cap = cv2.VideoCapture(1)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     while True:
 
@@ -21,12 +44,13 @@ def main():
 
         paper = extract_paper(frame)
 
-        lines = extract_lines(paper)
+        # lines = extract_lines(paper)
+        #
+        # skeleton = skeletonize(lines)
 
-        skeleton = skeletonize(lines)
 
-
-        cv2.imshow('skeleton', skeleton)
+        # img = mark_contours(frame)
+        cv2.imshow('OpenCV frame', paper)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- computer-vision/process_image.py
+++ computer-vision/process_image.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 """
@@ -19,7 +20,29 @@ Steps in image processing pipeline:
 # find coordinates of 3-4 corners of the paper and remove all components outside of it
 # for demo 1, assume flat a4 on contrasting surface
 def extract_paper(image):
-    final = image
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    hsv = cv2.GaussianBlur(hsv, (5,5), 2)
+
+    lower_red = np.array([20, 100, 0])
+    upper_red = np.array([340, 255, 255])
+
+    red = cv2.inRange(hsv, lower_red, upper_red)
+
+    contours, hierarchy = cv2.findContours(image=red, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+    sortedContours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+
+    paper_contour = []
+
+    if len(contours) > 1:
+        paper_contour = sortedContours[1]
+    else:
+        return image
+
+    x, y, w, h = cv2.boundingRect(paper_contour)
+    img = cv2.drawContours(image, [paper_contour], -1, (0,255,0), 3)
+
+    final = img
     return final;
 
 
